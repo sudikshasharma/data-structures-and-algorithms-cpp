036f2fe15057b8ba60e05310fb492aeb5dc118ca
Sudiksha Sharma <sudiksha.sd1@gmail.com>
2022-02-07 23:13:30 +0530

    Created Linlked list and implemented adding elements functionality

---
+++ LinkedList/LinkedListOperations_ListStl.cpp
@@ -0,0 +1,84 @@
+/*
+
+Cpp program to create a linked list and implement its basic operations like insertion, deletion etc. using oop.
+The given program is written in the way the actual class 'List' is defined in stl.
+
+*/
+
+#include <iostream>
+
+// Forward declaration (of List class) to avoid errors (as List class is defined after Node class. So, it'd give not declared error if Node class uses any member of List class).
+class List;
+
+class Node
+{
+    int data;
+    Node *next;
+
+public:
+    // Creating a Node (Calling Constructor)
+    Node(int val) : data(val), next(NULL) {}
+
+    // making List class a friend class of Node class (List class can access the private members of Node class)
+    friend class List;
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    // Creating LinkedList (Calling Constructor)
+    List() : head(NULL), tail(NULL) {}
+
+    // Pushing a new node in front
+    void push_front(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    // Pushing a new node at the end
+    void push_back(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+        return;
+    }
+
+    // Print the linked list
+    void print()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            std::cout << temp->data << "->";
+            temp = temp->next;
+        }
+    }
+};
+
+int main()
+{
+    List l;
+    std::cout << "Linked List :" << std::endl;
+    l.push_front(1);
+    l.push_front(2);
+    l.push_back(3);
+    l.print();
+    return 0;
+}
